216e8cb4c0ad9a1545c3e95e038838c13245e64a
Mamon Yoshioka <yoshiokamamon@gmail.com>
2024-10-05 12:47:44 +0900

    remove footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Top from './components/Top'
 import About from './components/About'
-import Footer from './components/Footer'
+
 
 
 const App = () => {
@@ -8,7 +8,6 @@ const App = () => {
     <>
       <Top />
       <About/>
-      <Footer/>
 
     </>
   )
